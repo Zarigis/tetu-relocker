b646fce83f1f6eb9de3dce0c5d415aa2d8335b87
Zarigis <zarigis@proton.me>
2023-01-04 22:13:53 -0800

    initial commit

---
+++ src/veTetuRelocker.sol
@@ -0,0 +1,133 @@
+
+
+// SPDX-License-Identifier: UNLICENSED
+
+
+pragma solidity ^0.8.13;
+
+//import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+import "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import "../lib/ops/contracts/integrations/OpsReady.sol";
+
+interface veTetu {
+  function increaseUnlockTime(uint _tokenId, uint _lockDuration) external returns (uint power, uint unlockDate);
+  function isApprovedOrOwner(address _spender, uint _tokenId) external view returns (bool);
+  function ownerOf(uint _tokenId) external view returns (address);
+  function lockedEnd(uint _tokenId) external view returns (uint);
+  function setApprovalForAll(address _operator, bool _approved) external;
+}
+
+interface WMatic {
+  function withdraw(uint256 wad) external;
+}
+
+contract veTetuRelocker is OpsReady {
+    address public constant VETETU = 0x6FB29DD17fa6E27BD112Bc3A2D0b8dae597AeDA4;
+    address public constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+    address public constant OPS = 0x527a819db1eb0e34426297b03bae11F2f8B3A19E;
+
+    uint internal constant MAX_TIME = 16 weeks;
+    uint internal constant WEEK = 1 weeks;
+
+    address public operator;
+    uint[] public veNFTs;
+    bool public paused = false;
+    
+    constructor(address _taskCreator) OpsReady(OPS, _taskCreator) {
+      operator = _taskCreator;
+    }
+
+    receive() external payable {}
+
+    function setOperator(address newOperator) external returns (bool) {
+      require(msg.sender == operator);
+      operator = newOperator;
+      return true;
+    }
+
+    function setPaused(bool _paused) external returns (bool) {
+      require(msg.sender == operator);
+      paused = _paused;
+      return true;
+    }
+
+    function rescueToken(address tok, uint amount) external returns (bool){
+      require(msg.sender == operator);
+      require(tok != VETETU);
+      IERC20(tok).transfer(operator, amount);
+      return true;
+    }
+
+    function register(uint veNFT) external returns (uint idx) {
+      // sender must own the NFT
+      require(veTetu(VETETU).isApprovedOrOwner(msg.sender, veNFT));
+      // this contract must be an approved operator of the NFT
+      require(veTetu(VETETU).isApprovedOrOwner(address(this), veNFT));
+      idx = veNFTs.length;
+      veNFTs.push(veNFT);
+    }
+
+    function unregister(uint idx) external returns (bool) {
+      uint veNFT = veNFTs[idx];
+      require(veTetu(VETETU).isApprovedOrOwner(msg.sender, veNFT));
+      veNFTs[idx] = veNFTs[veNFTs.length - 1];
+      veNFTs.pop();
+      return true;
+    }
+
+    function getReadyNFT() public view returns (bool success, uint veNFT) {
+      if (paused) {
+        return (false, 0);
+      }
+      address veOwner;
+      uint lockEnd;
+      uint targetTime = (block.timestamp + MAX_TIME) / WEEK * WEEK;
+      uint allowance;
+
+      for(uint i = 0; i < veNFTs.length; i++){
+        veNFT = veNFTs[i];
+        veOwner = veTetu(VETETU).ownerOf(veNFT);
+        lockEnd = veTetu(VETETU).lockedEnd(veNFT);
+        allowance = IERC20(WMATIC).allowance(veOwner, address(this));
+
+        if (targetTime > lockEnd && allowance > 0 && veTetu(VETETU).isApprovedOrOwner(address(this), veNFT)) {
+          return (true, veNFT);
+        }
+      }
+      return (false, 0);
+    }
+    
+    // for gelato resolver
+    function checker()
+        external
+        view
+        returns (bool canExec, bytes memory execPayload)
+    {
+        (bool success, uint veNFT) = getReadyNFT();
+        if (!success) {
+          return (false, bytes("No veNFTs ready"));
+        }
+
+        execPayload = abi.encodeCall(veTetuRelocker.processLock, (veNFT));
+        return (true, execPayload);
+    }
+
+    function processLock(uint veNFT) external onlyDedicatedMsgSender returns (bool) {
+      require(!paused);
+
+      address veOwner = veTetu(VETETU).ownerOf(veNFT);
+      (uint256 fee,address feeToken) = _getFeeDetails();
+      require(feeToken == ETH || (fee == 0 && feeToken == address(0)));
+
+      IERC20(WMATIC).transferFrom(veOwner, address(this), fee);
+      WMatic(WMATIC).withdraw(fee);
+      veTetu(VETETU).increaseUnlockTime(veNFT, MAX_TIME);
+
+      if (feeToken != address(0)){
+        _transfer(fee,feeToken);
+      }
+      return true;
+    }
+
+}
---
+++ test/veTetuRelockerTest.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import "../src/veTetuRelocker.sol";
+import "../lib/forge-std/src/Test.sol";
+import "../lib/forge-std/src/console.sol";
+
+contract veTetuRelockerTest is Test {
+    uint256 constant FORK_BLOCK_NUMBER = 37680341;
+    string MATIC_RPC_URL = vm.envString("MATIC_RPC_URL");
+
+    address public constant VETETU = 0x6FB29DD17fa6E27BD112Bc3A2D0b8dae597AeDA4;
+    address public constant USER = 0xa68444587ea4D3460BBc11d5aeBc1c817518d648;
+    address public constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+
+    function testLockerLogic() public {
+        uint veNFT = 96;
+
+        vm.createSelectFork(MATIC_RPC_URL, FORK_BLOCK_NUMBER);
+        veTetuRelocker c = new veTetuRelocker(USER);
+
+        vm.startPrank(USER);
+        veTetu(VETETU).increaseUnlockTime(veNFT, 16 weeks);
+        vm.warp(block.timestamp + 7 days);
+
+        veTetu(VETETU).setApprovalForAll(address(c), true);
+        IERC20(WMATIC).approve(address(c), type(uint256).max);
+        // get some WMATIC for tips
+        uint sendAmount = 100000000000000000;
+        (bool s,) = payable(WMATIC).call{value: sendAmount}("");
+        require (s);
+        c.register(veNFT);
+        vm.stopPrank();
+
+        console.log("Lockend Start:", veTetu(VETETU).lockedEnd(veNFT));
+
+        vm.startPrank(c.dedicatedMsgSender());
+        vm.warp(block.timestamp + 7 days);
+
+        (bool canExec, bytes memory payLoad) = c.checker();
+        require (canExec);
+        (bool ss, ) = address(c).call(payLoad);
+        require (ss);
+        (bool canExec2, bytes memory payLoad2) = c.checker();
+        require (!canExec2);
+        console.log("Now", block.timestamp);
+        vm.warp(block.timestamp + 7 days);
+
+        console.log("Now", block.timestamp);
+
+        (bool canExec3, bytes memory payLoad3) = c.checker();
+        require (canExec3);
+
+        (bool s3, ) = address(c).call(payLoad3);
+        require (s3);
+        
+        vm.stopPrank();
+        vm.startPrank(USER);
+        //veTetu(VETETU).increaseUnlockTime(veNFT, 16 weeks);
+
+
+        console.log("Lockend End:", veTetu(VETETU).lockedEnd(veNFT));
+
+    }
+}
